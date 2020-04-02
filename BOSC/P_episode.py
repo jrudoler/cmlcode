e8125e5bcc27e227f44343f836960fc773775b66
Joseph Rudoler <40604402+jrudoler@users.noreply.github.com>
2020-04-02 12:06:34 -0400

    update

--- BOSC/P_episode.py
+++ BOSC/P_episode.py
@@ -14,7 +14,7 @@ from sklearn.metrics import r2_score
 
 class P_episode(object):
     '''An object for handling the Better Oscillation Detection Methods
-    **Currently only handles encoding events**
+    **Currently only handles encoding events, compatible with FR1 data**
 
     Parameters:
         events - a pandas DataFrame containing the event or events (from the same session) being analyzed
